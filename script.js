f144b3c89c5c5f203b0703c036e7b6450893b4cd
Jospin <109992970+JospinJudes@users.noreply.github.com>
2024-07-28 16:26:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     fetch('2024-07-25 (1).csv')
-//         .then(response => response.text())
-//         .then(data => {
-//             Papa.parse(data, {
-//                 header: true,
-//                 complete: function(results) {
-//                     const table = document.getElementById('csvTable');
-//                     const headerRow = document.createElement('tr');
-
-//                     // Create table headers
-//                     for (const header in results.data[0]) {
-//                         const th = document.createElement('th');
-//                         th.textContent = header;
-//                         headerRow.appendChild(th);
-//                     }
-//                     table.appendChild(headerRow);
-
-//                     // Create table rows
-//                     results.data.forEach(row => {
-//                         const tr = document.createElement('tr');
-//                         for (const cell in row) {
-//                             const td = document.createElement('td');
-//                             td.textContent = row[cell];
-//                             tr.appendChild(td);
-//                         }
-//                         table.appendChild(tr);
-//                     });
-//                 }
-//             });
-//         });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     fetch('2024-07-25 (1).csv')
         .then(response => {
